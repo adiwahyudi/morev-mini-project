05c6c9cc1ac63451665b8a842adc03cefed5d509
I Wayan Adi Wahyudi <adi.wahyudi14@gmail.com>
2021-12-03 14:19:05 +0700

    fix search

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import styles from './SearchBar.module.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faSearch } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {faSearch } from '@fortawesome/free-solid-svg-icons'
 
-export default function SearchBar() {
-    return (
-        <div className={`${styles['search-bar']} px-3 pb-4`}>
-          <form className="d-flex" >
-              <input 
-                type="text"
-                placeholder="Search your movie here.."/>
-            <FontAwesomeIcon 
-              icon={faSearch} 
-              color="white" 
-              size="2x" 
-              style={{marginLeft:10}}
-            />
-          </form>
-        </div>
-    )
+export default function SearchBar(props) {
+
+  const {onChangeInput} = props;
+
+  let data;
+
+  return (  
+      <div className={`${styles['search-bar']} px-3 pb-4`}>
+        <form className="d-flex" >
+            <input 
+              value={data}
+              onChange={onChangeInput}
+              type="text"
+              placeholder="Search your movie here.."/>
+          {/* <FontAwesomeIcon 
+            icon={faSearch} 
+            color="white" 
+            size="2x" 
+            style={{marginLeft:10}}
+          /> */}
+        </form>
+      </div>
+  )
 }
